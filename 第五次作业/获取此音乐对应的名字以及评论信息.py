28dec469ce0fb9e19fb2d9c9b34071bc43fefaa7
meteor_glb <1824783344@qq.com>
2022-07-11 20:16:17 +0800

    初版

--- 第五次作业/获取此音乐对应的名字以及评论信息.py
+++ 第五次作业/获取此音乐对应的名字以及评论信息.py
@@ -1,9 +1,13 @@
+"""
+
+提交日期：2022-7-11
+提交人：18期爬虫-05-selenium作业-赵**
+
+"""
 import time
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as Ec
 from selenium.webdriver import ActionChains
 
 '''
